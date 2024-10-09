7964191baa9d346fb5d3a94bd3b3de2714ca3bcb
wave-rec <werther0704@gmail.com>
2024-10-09 23:11:20 +0900

    [mini project] 배포 수정

--- movieproject/vite.config.js
+++ movieproject/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   json: {
-    stringify: true // JSON 파일을 문자열로 불러오도록 설정
-  }
+    stringify: true
+  },
+  build: {
+    outDir: 'dist', 
+  },
 });
